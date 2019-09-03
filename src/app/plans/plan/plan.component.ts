cede5dd2595ecc1b86f741337527f66470c284c8
erick.silva <erick.silva@wooza.com.br>
2019-09-03 03:13:06 -0300

    ajustes finais

--- src/app/plans/plan/plan.component.ts
+++ src/app/plans/plan/plan.component.ts
@@ -3,8 +3,7 @@ import { Plan } from './plan.model'
 
 @Component({
   selector: 'wza-plan',
-  templateUrl: './plan.component.html',
-  styleUrls: ['./plan.component.sass']
+  templateUrl: './plan.component.html'
 })
 export class PlanComponent implements OnInit {
 
